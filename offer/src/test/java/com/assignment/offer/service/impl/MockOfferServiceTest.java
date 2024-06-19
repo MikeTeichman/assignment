4ad4a852be5e64db77098596471b4ac194eb2530
michael <michael@192.168.1.52>
2024-06-19 09:14:11 +0200

    Fix date format

--- offer/src/test/java/com/assignment/offer/service/impl/MockOfferServiceTest.java
+++ offer/src/test/java/com/assignment/offer/service/impl/MockOfferServiceTest.java
@@ -1,8 +1,9 @@
 package com.assignment.offer.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -11,24 +12,24 @@ import com.assignment.offer.model.Offer;
 
 public class MockOfferServiceTest {
 
-    private final MockOfferService mockOfferService = new MockOfferService();
+	private final MockOfferService mockOfferService = new MockOfferService();
 
-    @Test
-    public void testGetOffersByCustomerId() {
-        List<Offer> offers = mockOfferService.getOffersByCustomerId("customer1");
+	@Test
+	public void testGetOffersByCustomerId() {
+		List<Offer> offersCust1 = mockOfferService.getOffersByCustomerId("customer1");
 
-        assertEquals(2, offers.size());
+		assertEquals(3, offersCust1.size());
 
-        Offer offer1 = offers.get(0);
-        assertEquals("offer1", offer1.getId());
-        assertEquals("customer1", offer1.getCustomerId());
-        assertEquals(LocalDateTime.now().minusDays(5).getDayOfMonth(), offer1.getCreationDate().getDayOfMonth());
-        assertEquals(null, offer1.getRejectionDate());
+		Offer offer1 = offersCust1.get(0);
+		assertEquals("offer1", offer1.getId());
+		assertEquals("customer1", offer1.getCustomerId());
+		assertNull(offer1.getCreationDate());
+		assertNull(offer1.getRejectionDate());
 
-        Offer offer2 = offers.get(1);
-        assertEquals("offer2", offer2.getId());
-        assertEquals("customer1", offer2.getCustomerId());
-        assertEquals(LocalDateTime.now().minusDays(10).getDayOfMonth(), offer2.getCreationDate().getDayOfMonth());
-        assertEquals(LocalDateTime.now().minusDays(1).getDayOfMonth(), offer2.getRejectionDate().getDayOfMonth());
-    }
+		Offer offer2 = offersCust1.get(1);
+		assertEquals("offer2", offer2.getId());
+		assertEquals("customer1", offer2.getCustomerId());
+		assertEquals("14-06-2024", offer2.getCreationDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+		assertNull(offer2.getRejectionDate());
+	}
 }
